9220324854750f5fc39b51ba860fbdaae59fbc04
juanjosearangoacevedo-lgtm <juanjosearangoacevedo@gmail.com>
2025-12-07 17:39:08 -0500

    Refactor SQL queries for clarity and consistency

--- ProyectoFinal.sql
+++ ProyectoFinal.sql
@@ -3,57 +3,66 @@ SELECT COUNT(*) AS total_usuarios
 FROM usuario;
 
 --2
-SELECT comuna_id,
-       COUNT(*) AS cantidad_barrios
+SELECT COMUNA AS nombre_comuna,
+       COUNT(barrio) AS cantidad_barrios
 FROM comuna_barrio
-GROUP BY comuna_id;
+GROUP BY COMUNA;
 
 --3
-SELECT nombre AS "Título del Juego",
-       estudio AS "Empresa Desarrolladora"
+SELECT nombre AS 'Título del Juego',
+       estudio_dev AS 'Empresa Desarrolladora'
 FROM juego;
 
 --4
 SELECT u.primer_nombre,
        u.primer_apellido,
-       b.nombre_barrio
-FROM usuario u, comuna_barrio b
-WHERE u.barrio_id = b.id_comuna_barrio;
+       c.barrio
+FROM usuario u,
+     comuna_barrio c
+WHERE u.COMUNA_BARRIO_id = c.id;
+
 
 --5
-SELECT plataforma_id,
+SELECT PLATAFORMA_id,
        COUNT(*) AS cantidad_juegos
 FROM juego
-GROUP BY plataforma_id;
+GROUP BY PLATAFORMA_id;
 
 --6
-SELECT nombre_equipo AS "Squad",
-       horas_entrenamiento AS "Tiempo Jugado"
+SELECT nombre AS 'Squad',
+       horas AS 'Tiempo Jugado'
 FROM equipo_juego;
 
 --7
-SELECT e.nombre_equipo,
-       j.nombre AS nombre_juego
-FROM equipo_juego e, juego j
-WHERE e.juego_id = j.id_juego;
+SELECT e.nombre AS equipo,
+       j.nombre AS juego
+FROM equipo_juego e,
+     juego j
+WHERE e.JUEGO_id = j.id;
+
 
 --8
 SELECT tipo,
-       COUNT(*) AS cantidad
+       COUNT(*) AS cantidad_juegos
 FROM juego
 GROUP BY tipo;
 
+
 --9
-SELECT l.nombre_logro,
-       l.puntos,
-       j.nombre AS nombre_juego
-FROM logro_trofeo l, juego j
-WHERE l.juego_id = j.id_juego;
+SELECT l.nombre AS logro,
+       l.puntos_req,
+       j.nombre AS juego
+FROM logro_trofeo l,
+     juego j
+WHERE l.JUEGO_id = j.id;
+
 
 --10
-SELECT s.fecha_sesion,
+SELECT s.fecha_agenda,
        u.primer_nombre,
        u.primer_apellido
 FROM sesion_entrenamiento s,
      usuario u
-WHERE s.arbitro_id = u.id_usuario;
+WHERE s.arbitro = u.id;
+
+
